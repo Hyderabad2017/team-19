4b7e4482d0514b747055f035973be4dd88be69e0
nrcsiri <nrcsiri2016@gmail.com>
2017-07-16 00:41:26 +0530

    datareyrivel

--- phpdb.php
+++ phpdb.php
@@ -1,28 +1,34 @@
 <?php
-$servername = "localhost";
-$Student_Id = "$_POST['studentid']";
-$Name="$_POST['name']";
-$Gender="$_POST['gender']";
-$Age="$_POST['age']";
-$Parental_Status="$_POST['parentalstatus']";
-$Family_Type="$_POST['type']";
-$Siblings="$_POST['sibling']";
-$Birth_Order="$_POST['birth']";
-$Parent_Education="$_POST['peducation']";
-$Family_Income="$_POST['income']";
-$username="root";
-$password="";
-$dbname="learningcurve";
-$conn = new mysqli($servername,$username, $password).die("do");
-mysql_select_db($dbname);
+$db_username = 'root';
+$db_password = '';
+$db_name = 'learningcurve';
+$db_host = 'localhost';
 
-$sql = "INSERT INTO detailsStudent (Student_Id,Name,Gender,Age,Parental_Status,Family_Type,Siblings,Birth_Order,Parent_Education,Family_Income)
-VALUES ('$_POST(Student_Id)', '$_POST(Name)','$_POST(Gender)','$_POST(Age)',$_POST(Parental_Status)','$_POST(Family_Type)','$_POST(Siblings)','$_POST(Birth_Order)','$_POST(Parent_Education)','$_POST(Family_Income)');
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+//connect to MySql 
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name); 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else{
+echo "Connected successfully";
 }
-$conn->close();
+
+$StudentId = $_POST["studentid"];
+$Name=$_POST['name'];
+$Gender=$_POST['gender'];
+$Age=$_POST['age'];
+$Parental_Status=$_POST['parentalstatus'];
+$Family_Type=$_POST['family'];
+$Siblings=$_POST['sibling'];
+$Birth_Order=$_POST['birth'];
+$Parent_Education=$_POST['peducation'];
+$Family_Income=$_POST['income'];
+//INSERT INTO `demographics`(`SCID`, `STID`, `TID`, `SNAME`, `Gender`, `Age`, `ParentalStatus`, `FamilyType`, `Siblings`, `BirthOrder`, `ParentEducation`, `FamilyIncome`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12])
+
+
+$sql = "INSERT INTO demographics(SCID, STID, TID, SNAME, Gender, Age, ParentalStatus, FamilyType, Siblings, BirthOrder, ParentEducation, FamilyIncome)
+values(100,$StudentId,100, '$Name','$Gender','$Age','$Parental_Status','$Family_Type','$Siblings','$Birth_Order','$Parent_Education','$Family_Income')";
+
+echo $sql;
+$result = $conn->query($sql);
+ 
 ?> 
