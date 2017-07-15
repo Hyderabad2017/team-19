44f5085abee29da0963ffc67c3a8cfbcc4342aff
nrcsiri <nrcsiri2016@gmail.com>
2017-07-16 00:29:39 +0530

    dataretrivel

--- phpdb.php
+++ phpdb.php
@@ -4,21 +4,18 @@ $Student_Id = "$_POST['studentid']";
 $Name="$_POST['name']";
 $Gender="$_POST['gender']";
 $Age="$_POST['age']";
-$Parental_Status="$_POST['status']";
+$Parental_Status="$_POST['parentalstatus']";
 $Family_Type="$_POST['type']";
-$Siblings="$_POST['menu1']";
-$Birth_Order="$_POST['menu2']";
-$Parent_Education="$_POST['menu3']";
+$Siblings="$_POST['sibling']";
+$Birth_Order="$_POST['birth']";
+$Parent_Education="$_POST['peducation']";
 $Family_Income="$_POST['income']";
 $username="root";
 $password="";
 $dbname="learningcurve";
-$conn = new mysqli($servername,$username, $password);
+$conn = new mysqli($servername,$username, $password).die("do");
 mysql_select_db($dbname);
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-} 
+
 $sql = "INSERT INTO detailsStudent (Student_Id,Name,Gender,Age,Parental_Status,Family_Type,Siblings,Birth_Order,Parent_Education,Family_Income)
 VALUES ('$_POST(Student_Id)', '$_POST(Name)','$_POST(Gender)','$_POST(Age)',$_POST(Parental_Status)','$_POST(Family_Type)','$_POST(Siblings)','$_POST(Birth_Order)','$_POST(Parent_Education)','$_POST(Family_Income)');
 
