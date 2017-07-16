d55ca9ccec0bb17451202d34964b275c42e2b421
Satya Naraparaju <satyanaraparaju@gmail.com>
2017-07-16 06:28:42 +0530

    Broadcast Module

---
+++ SMS/sendit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+    include('way2sms-api.php');
+    sendWay2SMS ( '8142661344' , 'pandu123' , '9948353319' , 'Hello World');   
+    sendWay2SMS ( '8142661344' , 'pandu123' , '8790348115' , 'Hello World');   
+?>
+</body>
+</html>
