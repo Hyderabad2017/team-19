d301dcebf08d3a7afb0efa232e612b45710945f4
poojapawar1397 <pooja66166@gmaill.com>
2017-07-16 05:48:52 +0530

    second update

---
+++ process1.php
@@ -0,0 +1,108 @@
+
+<?php
+/*$db_username='root';
+$db_password='';
+$db_name='classinventory';
+$db_host='localhost';*/
+echo  "Data inserted";
+    /*if (isset($_POST['q1'])){
+        $q1 = $_POST['q1'];
+        mysql_query("INSERT INTO classinventory (q1) VALUES ('$q1')");
+
+    }
+    if (isset($_POST['q2'])){
+        $q2 = $_POST['q2'];
+        mysql_query("INSERT INTO classinventory (q2) VALUES ('$q2')");
+    }
+    if (isset($_POST['q3'])){
+        $q3 = $_POST['q3'];
+        mysql_query("INSERT INTO classinventory (q3) VALUES ('$q3')");
+    }
+    if (isset($_POST['q4'])){
+        $q4 = $_POST['q4'];
+        mysql_query("INSERT INTO classinventory (q4) VALUES ('$q4')");
+    }
+    if (isset($_POST['q5'])){
+        $q5 = $_POST['q5'];
+        mysql_query("INSERT INTO classinventory (q5) VALUES ('$q5')");
+    }
+    if (isset($_POST['q6'])){
+        $q6 = $_POST['q6'];
+        mysql_query("INSERT INTO classinventory (q6) VALUES ('$q6')");
+    }
+    if (isset($_POST['q7'])){
+        $q7 = $_POST['q7'];
+        mysql_query("INSERT INTO classinventory (q7) VALUES ('$q7')');
+    }
+    if (isset($_POST['q8'])){
+        $q8 = $_POST['q8'];
+        mysql_query('INSERT INTO classinventory (q8) VALUES ('$q8')');
+    }
+    if (isset($_POST['q9'])){
+        $q9 = $_POST['q9'];
+        mysql_query('INSERT INTO classinventory (q9) VALUES ('$q9')');
+    }
+    if (isset($_POST['q10'])){
+        $q10 = $_POST['q10'];
+        mysql_query('INSERT INTO classinventory(q10) VALUES ('$q10')');
+    }
+    
+    
+    if (isset($_POST['q11'])){
+        $q11 = $_POST['q11'];
+        mysql_query('INSERT INTO classinventory(q11) VALUES ('$q11')');
+    }
+    if (isset($_POST['q12'])){
+        $q12 = $_POST['q12'];
+        mysql_query('INSERT INTO classinventory(q12) VALUES ('$q12')');
+    }
+    if (isset($_POST['q13'])){
+        $q13 = $_POST['q13'];
+        mysql_query('INSERT INTO classinventory(q13) VALUES ('$q13')');
+    }
+    if (isset($_POST['q14'])){
+        $q14 = $_POST['q14'];
+        mysql_query('INSERT INTO classinventory(q14) VALUES ('$q14')');
+    }
+    if (isset($_POST['q15'])){
+        $q15 = $_POST['q15'];
+        mysql_query('INSERT INTO classinventory(q15) VALUES ('$q15')');
+    }
+    if (isset($_POST['q16'])){
+        $q16 = $_POST['q16'];
+        mysql_query('INSERT INTO classinventory(q16) VALUES ('$q16')');
+    }
+    if (isset($_POST['q17'])){
+        $q17 = $_POST['q17'];
+        mysql_query('INSERT INTO classinventory(q17) VALUES ('$q17')');
+    }
+    if (isset($_POST['q18'])){
+        $q18 = $_POST['q18'];
+        mysql_query('INSERT INTO classinventory(q18) VALUES ('$q18')');
+    }
+    if (isset($_POST['q19'])){
+        $q19 = $_POST['q19'];
+        mysql_query('INSERT INTO classinventory(q19) VALUES ('$q19')');
+    }
+    if (isset($_POST['q20'])){
+        $q20 = $_POST['q20'];
+        mysql_query('INSERT INTO classinventory(q20) VALUES ('$q20')');
+    }
+    if (isset($_POST['q21'])){
+        $q21 = $_POST['q21'];
+        mysql_query('INSERT INTO classinventory(q21) VALUES ('$q21')');
+    }
+    if (isset($_POST['q22'])){
+        $q22 = $_POST['q22'];
+        mysql_query('INSERT INTO classinventory(q22) VALUES ('$q22')');
+    }
+    if (isset($_POST['q23'])){
+        $q23 = $_POST['q23'];
+        mysql_query('INSERT INTO classinventory(q23) VALUES ('$q23')');
+    }
+    
+    if (isset($_POST['q25'])){
+        $q25 = $_POST['q25'];
+        mysql_query('INSERT INTO classinventory(q25) VALUES ('$q25')');
+    }*/
+    ?>
